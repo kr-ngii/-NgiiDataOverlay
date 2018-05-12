079f0ddf507b198c2f3255f8e9588da5a942f750
BJ Jang <jangbi882@gmail.com>
2018-05-12 16:33:49 +0900

    과거 코드로 온맵 로드

--- NgiiDataUtils/ngii_data_utils.py
+++ NgiiDataUtils/ngii_data_utils.py
@@ -175,7 +175,7 @@ class NgiiDataUtils:
             #    removed on close (see self.onClosePlugin method)
             if self.dockwidget == None:
                 # Create the dockwidget (after translation) and keep reference
-                self.dockwidget = NgiiDataUtilsDockWidget()
+                self.dockwidget = NgiiDataUtilsDockWidget(self.iface)
 
             # connect to provide cleanup on closing of dockwidget
             self.dockwidget.closingPlugin.connect(self.onClosePlugin)
--- NgiiDataUtils/ngii_data_utils_dockwidget.py
+++ NgiiDataUtils/ngii_data_utils_dockwidget.py
@@ -23,29 +23,75 @@
 
 import os
 
+from PyQt4.QtCore import *
+from PyQt4.QtGui import *
 from PyQt4 import QtGui, uic
-from PyQt4.QtCore import pyqtSignal
 from ngii_data_utils_dockwidget_base import Ui_NgiiDataUtilsDockWidgetBase
+from OnMap import OnMapLoader
+from Dxf import DxfLoader
 
-# FORM_CLASS, _ = uic.loadUiType(os.path.join(
-#     os.path.dirname(__file__), 'ngii_data_utils_dockwidget_base.ui'))
+FORM_CLASS, _ = uic.loadUiType(os.path.join(
+    os.path.dirname(__file__), 'ngii_data_utils_dockwidget_base.ui'))
 
 
 # class NgiiDataUtilsDockWidget(QtGui.QDockWidget, FORM_CLASS):
 class NgiiDataUtilsDockWidget(QtGui.QDockWidget, Ui_NgiiDataUtilsDockWidgetBase):
     closingPlugin = pyqtSignal()
+    OnMapLoader = None
 
-    def __init__(self, parent=None):
+    def __init__(self, iface, parent=None):
         """Constructor."""
         super(NgiiDataUtilsDockWidget, self).__init__(parent)
-        # Set up the user interface from Designer.
-        # After setupUI you can access any designer object by doing
-        # self.<objectname>, and you can use autoconnect slots - see
-        # http://qt-project.org/doc/qt-4.8/designer-using-a-ui-file.html
-        # #widgets-and-dialogs-with-auto-connect
         self.setupUi(self)
+
+        self.iface = iface
+        self._connect_action()
 
     def closeEvent(self, event):
         self.closingPlugin.emit()
         event.accept()
 
+    def _connect_action(self):
+        self.connect(self.btnAutoDetect, SIGNAL("clicked()"), self._on_click_btnAutoDetect)
+        self.connect(self.btnLoadVector, SIGNAL("clicked()"), self._on_click_btnLoadVector)
+        self.connect(self.btnLoadImage, SIGNAL("clicked()"), self._on_click_btnLoadImage)
+        self.connect(self.btnLoadTms, SIGNAL("clicked()"), self._on_click_btnLoadTms)
+        self.connect(self.btnLoadBaseMap, SIGNAL("clicked()"), self._on_click_btnLoadBaseMap)
+        self.connect(self.btnLoadOnmapBaseMap, SIGNAL("clicked()"), self._on_click_btnLoadOnmapBaseMap)
+        self.connect(self.btnLoadInternetBaseMap, SIGNAL("clicked()"), self._on_click_btnLoadInternetBaseMap)
+        self.connect(self.btnReportError, SIGNAL("clicked()"), self._on_click_btnReportError)
+
+    def _on_click_btnAutoDetect(self):
+        pass
+
+    def _on_click_btnLoadVector(self):
+        # 기존 폴더 유지하게 옵션 추가: https://stackoverflow.com/questions/23002801/pyqt-how-to-make-getopenfilename-remember-last-opening-path
+        vectorPath = QFileDialog.getOpenFileName(caption=u"국토지리정보 벡터파일 선택", filter=u"온맵(*.pdf);;지형도(*.dxf);;공공측량성과(*.dxf)", options=QFileDialog.DontUseNativeDialog)
+        if vectorPath is None:
+            return
+
+        filename, extension = os.path.splitext(vectorPath)
+
+        if extension.lower() == ".pdf":
+            OnMapLoader.load(vectorPath, self.iface)
+        elif extension.lower() == ".dxf":
+            DxfLoader.load(vectorPath)
+
+
+    def _on_click_btnLoadImage(self):
+        pass
+
+    def _on_click_btnLoadTms(self):
+        pass
+
+    def _on_click_btnLoadBaseMap(self):
+        pass
+
+    def _on_click_btnLoadOnmapBaseMap(self):
+        pass
+
+    def _on_click_btnLoadInternetBaseMap(self):
+        pass
+
+    def _on_click_btnReportError(self):
+        pass
