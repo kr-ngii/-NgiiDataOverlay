ec1b94ed3fcde0f7e2a8a3c0722671dde8a49d73
BJ Jang <jangbi882@gmail.com>
2018-05-20 15:17:58 +0900

    메타데이터 수정, DXF 준비

--- NGIIDataOverlayQualityCheck/Dxf/dxf_loader.py
+++ NGIIDataOverlayQualityCheck/Dxf/dxf_loader.py
@@ -1,10 +1,32 @@
 # -*- coding: utf-8 -*-
 
+import os
+from qgis.core import *
+from osgeo import ogr
+
 
 class DxfLoader():
     iface = None
     parent = None
 
-    def __init__(self, iface, parent):
+    def __init__(self, iface, filePath):
         self.iface
         self.parent
+
+    def runImport(self, filePath):
+        srcDriver = ogr.GetDriverByName("DXF")
+
+        # 그룹부터 만들고
+        filename, _ = os.path.splitext(os.path.basename(filePath))
+        root = QgsProject.instance().layerTreeRoot()
+        mainGroup = root.addGroup(filename)
+
+        # 하나씩 임포트
+        try:
+            # TODO: 좌표계는 일단 무조건 5186으로
+            layer = srcDriver.Open(filePath, 0)
+            QgsMapLayerRegistry.instance().addMapLayer(layer, False)
+            mainGroup.insertChildNode(1, QgsLayerTreeLayer(layer))
+
+        except Exception as e:
+            raise e
